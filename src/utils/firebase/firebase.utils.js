f6852a78ffa70ba128662a67db7acd231a28b74b
JonathanGRichards <110139851+JonathanGRichards@users.noreply.github.com>
2025-08-18 23:26:52 +0100

    add navigation and sign in components and firebase utils implementing firebase libraries

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,57 @@
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCL4DObZtvlopi5BmFDYNRtYsoCZRriPN8',
+  authDomain: 'king-clothing-db-1e79b.firebaseapp.com',
+  projectId: 'king-clothing-db-1e79b',
+  storageBucket: 'king-clothing-db-1e79b.firebasestorage.app',
+  messagingSenderId: '845655603123',
+  appId: '1:845655603123:web:0b268bd45ede5ad919bdc6',
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({
+  prompt: 'select_account',
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  if (!userAuth) return;
+
+  const userDocRef = doc(db, 'users', userAuth.uid);
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userDocRef;
+}
